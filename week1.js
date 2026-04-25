a4257b34873a6ac9aec76787fc2f8303253645db
LandonM-byui <mah22009@byui.edu>
2026-04-25 12:56:35 -0600

    WO1 Ponder: JS Variables and Constants

---
+++ week1.js
@@ -0,0 +1,31 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result)
+
+result = one + Number(two);
+console.log(result);
+
+let course = "CSE 131";
+if (true){
+    let studnet = "John";
+    console.log(course);
+    console.log(studnet);
+}
+
+console.log(course);
+console.log(studnet);
